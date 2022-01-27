d8a95e9fdae4967657595cc7cee00caaf3b89356
fcandiotti <fernando_candiotti@hotmail.com>
2022-01-27 05:50:47 -0300

    projeto clonado sds2 para desafio tdd

---
+++ src/main/java/com/devsuperior/Desafiotdd/DesafiotddApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.Desafiotdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafiotddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafiotddApplication.class, args);
+	}
+
+}
